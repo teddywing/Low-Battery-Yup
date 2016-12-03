ce4e6e5cae1b1144d6bdd95c6ed2ce663ec31e1c
Teddy Wing <git@teddywing.com>
2016-12-03 01:06:52 -0500

    Add copyright and license text to all source files

--- Low Battery Yup d/AppDelegate.m
+++ Low Battery Yup d/AppDelegate.m
@@ -2,8 +2,24 @@
 //  AppDelegate.m
 //  Low Battery Yup.d
 //
-//  Created by TW on 11/16/16.
-//  Copyright (c) 2016 TW. All rights reserved.
+//  Created by Teddy Wing on 11/16/16.
+//  Copyright (c) 2016 Teddy Wing. All rights reserved.
+//
+//  This file is part of Low Battery Yup.
+//
+//  Low Battery Yup is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  Low Battery Yup  is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Low Battery Yup. If not, see
+//  <http://www.gnu.org/licenses/>.
 //
 
 #import "AppDelegate.h"
